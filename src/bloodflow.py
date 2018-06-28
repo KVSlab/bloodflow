91569f0c382d9338c7ef00a1c9b756134aea4a9b
syverda <syverda@icloud.com>
2018-06-28 16:00:32 +0200

    Further experimented with data structures, added data/ folder to .gitignore.

--- src/bloodflow.py
+++ src/bloodflow.py
@@ -5,30 +5,45 @@ Author : Syver Døving Agdestein
 
 from fenics import *
 import numpy as np
+import matplotlib.pyplot as plt
 
-T = 1.0
-L = 20.0
-Nt = 100
-Nx = 100
+data_q = np.genfromtxt('../data/example_inlet.csv', delimiter = ',')
+print(data_q)
+plt.plot(data_q[:,0], data_q[:,1])
+plt.savefig('data.png')
 
-r0 = 1.0
+L, Tfinal = 20.8, data_q[-1,0]
+Nx, Nt = 100, len(data_q[:,0])
+
+r0 = 0.37
 E = 1.0
 H = 0.01
 Re = 1.0
-Deltab = 0.1
+nu = 1.0
+T = 1.0
+deltab = np.sqrt(nu*T/2/np.pi)
+A0 = pi*pow(r0,2)
 
 f = 4*E*H/3/r0
 
-mesh = Rectangle(Point(0,0), Point(T,L), Nt Nx)
-V = FunctionSpace(mesh, 'P', degree = 1)
+mesh = RectangleMesh(Point(0,0), Point(L, Tfinal), Nx, Nt)
+mesht = IntervalMesh(Nt,0,Tfinal)
+V = FunctionSpace(mesh, 'P', 1)
+W = FunctionSpace(mesht, 'P', 1)
 
-A = Function(V)
+R = Function(V)
 q = Function(V)
+A = pi*pow(R,2)
+
+q_inlet = project(data_q, W)
+
+
+U = as_vector([A,q])
+
+F = as_vector([q, pow(q,2)/A + sqrt(A0*A)])
+
+S = as_vector([0, 2*pi*R/deltab/Re*q/A])
 
 
 
-U = (A,q)
-
-F = (q, pow(q,2)/A + sqrt(A0*A))
-
-S = (0, 2*pi*R/deltab/Re*q/A
+bc_q = DirichletBC(V, ql, on_boundary and near(x[0], 0))
