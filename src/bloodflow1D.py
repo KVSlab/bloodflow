c4eac2f4b34bfca6bbc5864616379978746695d0
syverda <syverda@icloud.com>
2018-07-05 16:28:23 +0200

    Removed matrix stocking of values (to be implemented in post-processing).

--- src/bloodflow1D.py
+++ src/bloodflow1D.py
@@ -140,9 +140,6 @@ A0 = Constant(pi*pow(r0,2))
 
 
 
-
-
-
 R1 = 25300
 R2 = 13900
 CT = 1.3384e-6
@@ -191,9 +188,6 @@ def outlet_area(U_n, k_max = 100, tol = 1.0e-7):
 	return Am0
 
 
-
-
-
 # Definition of trial functions
 U = Function(V2)
 A, q = split(U)
@@ -226,20 +220,8 @@ def outlet_bdry(x, on_boundary):
 bc_outlet = DirichletBC(V2.sub(0), A_out, outlet_bdry)
 bc_inlet = DirichletBC(V2.sub(1), q_in, inlet_bdry)
 
-#bcs = [bc_inlet]
 bcs = [bc_inlet, bc_outlet]
 
-
-""" # Before integration by parts
-# Variational form: FF == 0
-FF = A*v1*dx\
-   + q*v2*dx\
-   + dt*grad(q)[0]*v1*dx\
-   + dt*grad(pow(q,2)/(A+1.e-16)+f*sqrt(A0*(A+1.e-16)))[0]*v2*dx\
-   + dt*2*sqrt(pi)/db/Re*q/sqrt(A+1.e-16)*v2*dx\
-   - U_n[0]*v1*dx\
-   - U_n[1]*v2*dx
-"""
 
 # Variational form: FF == 0
 # In order for FEniCS to handle the expression correctly, a small perturbation of A seems necessary. Since A > 0.4, this should not numerically change the solution.
@@ -256,17 +238,15 @@ FF = A*v1*dx\
 
 
 # Number of cardiac cycles
-N_cycles = 4
+N_cycles = 1
 
-# Matrices for storing the solution
-qmat = np.zeros([Nx, N_cycles*Nt])
-Amat = np.zeros([Nx, N_cycles*Nt])
-pmat = np.zeros([Nx, N_cycles*Nt])
+# File for storing the solution
+xdmffile_A = XDMFFile(mpi_comm_world(), '../output/constant_r0/area.xdmf')
+xdmffile_q = XDMFFile(mpi_comm_world(), '../output/constant_r0/flow.xdmf')
 
-qmat[:,0] = qq[0]*np.ones(Nx)
-Amat[:,0] = A0(0)*np.ones(Nx)
-
-#xdmffile_U = XDMFFile('../output/constant_r0/bloodflow1D.xdmf')
+# Initial values (t_0)
+xdmffile_A.write_checkpoint(U_n.split()[0], 'area', 0)
+xdmffile_q.write_checkpoint(U_n.split()[1], 'flow', 0)
 
 # Progress bar
 progress = Progress('Time-stepping')
@@ -274,15 +254,15 @@ set_log_level(PROGRESS)
 
 t = 0
 
-
-
 # Time-stepping
 for n_cycle in range(N_cycles):
 
 	for n in range(0,Nt-1):
 		
 		print('Iteration '+str(n))
-
+		
+		t += dt
+		
 		# Update inlet boundary condition
 		q_in.assign(Constant(qq[n+1]))
 		
@@ -296,67 +276,17 @@ for n_cycle in range(N_cycles):
 		# Update previous solution
 		U_n.assign(U)
 		
-		#xdmffile_U.write(U, dt)
-		
-		# Store solution at time t_n+1
-		qmat[:,n_cycle*Nt+n+1] = [q([x]) for x in xx]
-		Amat[:,n_cycle*Nt+n+1] = [A([x]) for x in xx]
+		# Update XDMF-files
+		xdmffile_A.write_checkpoint(U.split()[0], 'area', t)
+		xdmffile_q.write_checkpoint(U.split()[1], 'flow', t)
 		
 		# Update progress bar
 		progress.update((n_cycle*Nt+(n+1))/Nt/N_cycles*T)
 		
 	U_n.assign(U)
-	
-	if n_cycle < N_cycles - 1:
-		qmat[:,(n_cycle+1)*Nt] = [q([x]) for x in xx]
-		Amat[:,(n_cycle+1)*Nt] = [A([x]) for x in xx]
 
-
-
-X = np.linspace(0, N_cycles*T, N_cycles*Nt)
-
-X, Y = np.meshgrid(X, xx)
-
-# Assembly of the pressure matrix
-pmat = unit_to_mmHg(p0 + f*(1-np.sqrt(A0(0)/Amat)))
-
-
-# Area plot
-fig = plt.figure(figsize=(8,6))
-ax = fig.gca(projection='3d')
-surf = ax.plot_surface(X, Y, Amat, rstride=1, cstride=1,  cmap='viridis', linewidth=0, antialiased=False)
-ax.set_xlabel('t')
-ax.set_ylabel('x')
-ax.set_zlabel('A')
-ax.set_ylim(min(xx), max(xx))
-ax.set_xlim(min(tt), N_cycles*max(tt))
-plt.savefig('../output/constant_r0/area.png')
-
-
-# Flow plot
-fig = plt.figure(figsize=(8,6))
-ax = fig.gca(projection='3d')
-surf = ax.plot_surface(X, Y, qmat, rstride=1, cstride=1,  cmap='viridis', linewidth=0, antialiased=False)
-ax.set_xlabel('t')
-ax.set_ylabel('x')
-ax.set_zlabel('q')
-ax.set_ylim(min(xx), max(xx))
-ax.set_xlim(min(tt), N_cycles*max(tt))
-#ax.set_zlim(-15,0.0)
-plt.savefig('../output/constant_r0/flow.png')
-
-
-# Pressure plot
-fig = plt.figure(figsize=(8,6))
-ax = fig.gca(projection='3d')
-surf = ax.plot_surface(X, Y, pmat, rstride=1, cstride=1,  cmap='viridis', linewidth=0, antialiased=False)
-ax.set_xlabel('t')
-ax.set_ylabel('x')
-ax.set_zlabel('p')
-ax.set_ylim(min(xx), max(xx))
-ax.set_xlim(min(tt), N_cycles*max(tt))
-plt.savefig('../output/constant_r0/pressure.png')
-
+xdmffile_A.close()
+xdmffile_q.close()
 
 
 
