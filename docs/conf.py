0ff75033f919ff50106815d2982307c3f1b0ebb3
akdiem <alexandra.diem@gmail.com>
2018-10-04 15:48:18 +0200

    added mock importing to conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -16,6 +16,8 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('..'))
 
+autodoc_mock_imports = ['dolfin', 'matplotlib', 'mpl_toolkits', 'scipy']
+
 
 # -- Project information -----------------------------------------------------
 
