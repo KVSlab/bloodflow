f11db88f398b03879ba0b55f90ed8f95cd17893c
akdiem <alexandra.diem@gmail.com>
2018-10-26 09:13:47 +0200

    forgot comma after numpydoc module load

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -39,7 +39,7 @@ release = '0.1.0'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx.ext.numpydoc'
+    'sphinx.ext.numpydoc',
     'sphinx.ext.autodoc',
     'sphinx.ext.mathjax',
     'sphinx.ext.viewcode',
